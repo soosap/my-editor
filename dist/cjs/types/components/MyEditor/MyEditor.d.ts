cbcd8ace1894f8bd7ad681a293bb8f1deb0f1a5e
soosap <prasath.soosaithasan@protonmail.ch>
2023-03-08 22:36:27 +0100

    build(gitignore): add dist to gitignore

--- dist/cjs/types/components/MyEditor/MyEditor.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="react" />
-export interface Props {
-    className?: string;
-}
-export declare const MyEditor: ({ className }: Props) => JSX.Element;
-export default MyEditor;
--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="react" />
-type Size = 'xs' | 'sm' | 'md' | 'lg' | 'xl' | '2xl' | '3xl';
-
-interface Props {
-    className?: string;
-}
-declare const MyEditor: ({ className }: Props) => JSX.Element;
-
-export { MyEditor, Size };
