8a239922de1d5e001a06a4489b0e6de4353d0b08
soosap <prasath.soosaithasan@protonmail.ch>
2023-03-09 23:27:34 +0100

    feat(MyEditor): implement minimal capability lexical editor

--- src/components/MyEditor/MyEditor.tsx
+++ src/components/MyEditor/MyEditor.tsx
@@ -1,9 +1,46 @@
+import {
+  LexicalComposer,
+  type InitialEditorStateType,
+  type InitialConfigType,
+} from '@lexical/react/LexicalComposer'
+import { ContentEditable } from '@lexical/react/LexicalContentEditable'
+import LexicalErrorBoundary from '@lexical/react/LexicalErrorBoundary'
+import { RichTextPlugin } from '@lexical/react/LexicalRichTextPlugin'
+import { HistoryPlugin } from '@lexical/react/LexicalHistoryPlugin'
+
 export interface Props {
+  name: string
   className?: string
+  initialEditorState?: InitialEditorStateType
+  placeholder?: string
 }
 
-export const MyEditor = ({ className }: Props) => {
-  return <div className={className}>MyEditor</div>
+export const MyEditor = ({
+  className,
+  name,
+  initialEditorState,
+  placeholder = 'Enter some text...',
+}: Props) => {
+  const editorConfig: InitialConfigType = {
+    namespace: name,
+    editorState: initialEditorState,
+    onError(error) {
+      throw error
+    },
+  }
+
+  return (
+    <LexicalComposer initialConfig={editorConfig}>
+      <div className={className}>
+        <RichTextPlugin
+          contentEditable={<ContentEditable />}
+          placeholder={<div>{placeholder}</div>}
+          ErrorBoundary={LexicalErrorBoundary}
+        />
+        <HistoryPlugin />
+      </div>
+    </LexicalComposer>
+  )
 }
 
 export default MyEditor
